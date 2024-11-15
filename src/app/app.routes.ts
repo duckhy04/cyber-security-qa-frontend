8c0c0932e70150c84baf39a3c6d57a8e97c1d3b0
duckhy04 <nguynduchuy12@gmail.com>
2024-11-16 01:19:00 +0700

    Add component user/admin/auth and router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/auth', pathMatch: 'full' },
+
+    // Router for auth
+    {
+        path: 'auth',
+        loadComponent: () => import('../app/components/auth/auth.component').then((m) => m.AuthComponent)
+    },
+
+    // Router for admin
+    {
+        path: 'admin',
+        loadComponent: () => import('../app/components/admin/admin.component').then((m) => m.AdminComponent)
+    },
+
+    // Router for user
+    {
+        path: 'user',
+        loadComponent: () => import('../app/components/user/user.component').then((m) => m.UserComponent)
+    },
+];
